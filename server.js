7136da00b968559cdda47d09d90855eee550e6e9
warayutpetch <31350256+warayutpetch@users.noreply.github.com>
2023-12-18 13:16:00 +0700

    commit test

---
+++ server.js
@@ -0,0 +1,22 @@
+// Import the express module
+var express = require("express");
+
+// Initialize express
+var app = express();
+
+// Home page
+app.get("/", (req, res, next) => {
+    res.set('Content-Type', 'text/html');
+    res.send('<html><body><h1>Home Page</h1></body></html>');
+});
+
+// On path /api/ we serve JSON content
+app.get("/api", (req, res, next) => {
+    res.set('Content-Type', 'application/json');
+    res.send('{"name":"John","age":30,"cars":["Ford", "BMW", "Fiat"]}');
+});
+
+// The server start listening on port 3001
+app.listen(3001, () => {
+    console.log("Server running on port 3001");
+});
